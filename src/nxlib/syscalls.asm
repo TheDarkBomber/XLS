ff07114930928628914718f8e10cba852bd072b8
TheDarkBomber <thedarkbomber80@gmail.com>
2023-06-29 18:21:27 +0100

    NXLib: Work on getting high-level I/O working.
    Also add a debug mode for building NXLib with.

--- src/nxlib/syscalls.asm
+++ src/nxlib/syscalls.asm
@@ -10,8 +10,11 @@ global %1:function
 section .text
 defsyscall READ, 0
 defsyscall WRITE, 1
+defsyscall OPEN, 2
+defsyscall CLOSE, 3
 defsyscall BRK, 12
 defsyscall GETPID, 39
 defsyscall FORK, 57
 defsyscall EXIT, 60
 defsyscall TERMINATE, 62
+defsyscall FSYNC, 74
